f5733899f6090c30b29d836dc2b353d8fa0fed38
Godchaama <gmod2015@yahoo.com.vn>
2024-10-22 08:13:34 +0700

    fixed bug

--- BaiTap10.py
+++ BaiTap10.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
--- BaiTap12.py
+++ BaiTap12.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver import ActionChains
 import time
 import pandas as pd
