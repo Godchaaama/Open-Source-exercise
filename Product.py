57c0c302a510c5c8ae4d3ffbe4fd3cc69055d5a8
Godchaama <gmod2015@yahoo.com.vn>
2024-10-22 08:30:14 +0700

    fixed

--- Product.py
+++ Product.py
@@ -3,45 +3,12 @@ from selenium.webdriver.common.by import By
 import time
 import pandas as pd
 import re
-import sqlite3
 
-#tao db
-
-conn = sqlite3.connect("Product.db")
-c = conn.cursor()
-try:
-    c.execute('''
-        CREATE TABLE Product (
-            name primary key,
-            Gia_Goc text,
-            Gia_Giam text
-        )
-    ''')
-except Exception as e:
-    print(e)
-
+i = 1
 #Truy cap chrome
 driver = webdriver.Chrome()
 url = 'https://gochek.vn/collections/all'
 
-def add_data(a,b,c):
-    # Kết nối và lấy dữ liệu
-    conn = sqlite3.connect('Product.db')
-    c = conn.cursor()
-    #thực hiện câu lệnh thêm
-    c.execute('''
-        INSERT INTO 
-        Product (name, Gia_Goc, Gia_Giam)
-        VALUES 
-        (:name, :Gia_Goc, :Gia_Giam)
-    ''',{
-        'name' : a,
-        'Gia_Goc' : b,
-        'Gia_Giam': c,
-      }
-    )
-    conn.commit()
-    conn.close()
 
 #mở tràn web
 driver.get(url)
@@ -51,7 +18,9 @@ time.sleep(3)
 products = driver.find_elements(By.CLASS_NAME, "col-md-3.col-sm-4.col-xs-6.pro-loop.col_fix20")
 print(len(products))
 
+
 for product in products: 
+    stt = i 
     #tim ten
     try: 
         name_element = product.find_element(By.TAG_NAME, "h3").text
@@ -59,19 +28,17 @@ for product in products:
         print("error")
 
     #tim gia goc va gia giam
-    try:
         org_price = product.find_element(By.TAG_NAME, 'p').text
         org_pr,dis_price = org_price.split()
-        add_data(name_element, org_pr, dis_price)
-        print(org_pr, "|", dis_price)
-        print("org_prc", type(org_pr))
+    
 
-    except:
-        org_pr = org_price.split()
-        true_org_prc = org_pr[0]
-        dis_price = "NULL"
-        #them vao database
-        add_data(name_element, true_org_prc, dis_price)
+df = pd.DataFrame({
+    "STT": stt,
+    "ten": name_element,
+    "gia goc": org_pr,
+    "Gia giam": dis_price
+})
 
+df.to_excel("danh_sach_sp.xlsx", index= False)
 
 driver.quit()
